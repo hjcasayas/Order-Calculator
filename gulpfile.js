863c344f799ff8dbee41b0f14fa4a8eb77efea95
hjcasayas <henlyjade.casayas@gmail.com>
2018-11-25 21:56:33 +0800

    finished initial design for navigation and large-hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
+// Variable Initilization
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+browserSync = require('browser-sync').create(),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
+// Tasks
 gulp.task('default', function(){
   console.log('Horray!');
 });
 
 gulp.task('watch', function(){
+  
+  watch('./app/assets/styles/**/*.css', styles);
+  watch('./app/temp/styles/styles.css', css_inject);
 
   browserSync.init({
     server: {
@@ -19,3 +31,19 @@ gulp.task('watch', function(){
   });
 
 });
+
+// Functions
+function styles() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, hexrgba, nested, autoprefixer]))
+		.on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end');
+    })    
+    .pipe(gulp.dest('./app/temp/styles'));
+}
+
+function css_inject() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+}
