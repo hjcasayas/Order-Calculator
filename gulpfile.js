b334536ef05dff0edb6c5acf05d38fc3051196b7
hjcasayas <henlyjade.casayas@gmail.com>
2018-12-16 21:23:52 +0800

    Finished with gulp and babel

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,29 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba');
+hexrgba = require('postcss-hexrgba'),
+webpack = require('webpack');
 
 // Tasks
-gulp.task('default', function(){
-  console.log('Horray!');
-});
-
 gulp.task('watch', function(){
   
-  watch('./app/assets/styles/**/*.css', styles);
-  watch('./app/temp/styles/styles.css', css_inject);
-
   browserSync.init({
+    notify: false,
     server: {
       baseDir: "app"
     }
   });
 
-  watch('./app/*.html', function(){
-    browserSync.reload();
-  });
+  watch('./app/assets/styles/**/*.css', gulp.series(styles, css_inject));
+  watch('./app/assets/scripts/**/*.js', gulp.series(scripts, browserReload));
+
+  watch('./app/*.html', browserReload);
 
 });
 
 // Functions
+
+// styles
 function styles() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, mixins, cssvars, hexrgba, nested, autoprefixer]))
@@ -46,4 +44,21 @@ function styles() {
 function css_inject() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+}
+
+// scripts
+
+function scripts(callback) {
+  webpack(require('./webpack.config'), function(err, stats) {
+    if (err) {
+      console.log(err.toString())
+    }  
+    console.log(stats.toString());
+    callback();
+  });
+}
+
+// for html and scripts reload
+function browserReload() {
+  browserSync.reload();
 }
